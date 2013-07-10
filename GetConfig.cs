8bcf78f54424c414548e0e1a75ce3d22e867a830
dylan2012 <dylan.huang@ischool.com.tw>
2013-07-10 09:56:54 +0800

    建立第一個匯出功能版本

---
+++ GetConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FISCA.DSAUtil;
+using Framework.Feature;
+using System.Xml;
+using SHSchool.Data;
+using Campus.DocumentValidator;
+
+namespace ExportExample
+{
+    static public class GetConfig
+    {
+        /// <summary>
+        /// 取得文字評量代碼表清單
+        /// </summary>
+        static public List<string> GetWordCommentList()
+        {
+            List<string> faceList = new List<string>();
+            DSXmlHelper helper = Config.GetWordCommentList().GetContent();
+            foreach (XmlElement morality in helper.GetElements("Content/Morality"))
+            {
+                DSXmlHelper moralityHelper = new DSXmlHelper(morality);
+                string face = moralityHelper.GetText("@Face");
+                if (!faceList.Contains(face))
+                {
+                    faceList.Add(face);
+                }
+            }
+            return faceList;
+        }
+    }
+}
